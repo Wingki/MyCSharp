0b05984e73e4a864d0c82dafb192485b43bee8c7
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-03-04 14:50:54 +1000

    Updated code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Person
+{
+    // Properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // Constructor
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Method
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an object of Person class
+        Person person1 = new Person("Alice", 25);
+        person1.Introduce();
+    }
+}
